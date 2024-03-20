6552f410c118cd9d224768d8541a26e9939dba83
Stingers-6642 <129606137+Stingers-6642@users.noreply.github.com>
2024-03-19 21:20:53 -0400

    Update crescendo_pit_config.js

--- 2024/crescendo_pit_config.js
+++ 2024/crescendo_pit_config.js
@@ -69,7 +69,7 @@ var config_data = `
       "code": "fpu",
       "type": "bool"
     },
-    { "name": "Autos",
+    { "name": "How Many Autonomous Programs? Can you Score From Any Position?",
       "code": "aut",
       "type": "text",
       "size": 20,
