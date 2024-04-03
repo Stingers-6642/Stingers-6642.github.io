f813f28fd2283115a6b46401279f976fea290f22
Stingers-6642 <129606137+Stingers-6642@users.noreply.github.com>
2024-04-02 20:53:18 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -112,14 +112,12 @@ var config_data = `
       "code": "fs",
       "type":"radio",
       "choices": {
-        "p": "Parked<br>",
-        "o": "Onstage<br>",
-        "s": "Onstage (Spotlit)<br>",
-        "h": "Harmony<br>",
-        "a": "Attempted but failed<br>",
-        "x": "Not attempted"
+        "1": "Parked<br>",
+        "3": "Onstage<br>",
+        "4": "Harmony<br>",
+        "0": "Not attempted"
       },
-      "defaultValue": "x"
+      "defaultValue": "0"
     },
     { "name": "Note in Trap",
       "code": "nit",
